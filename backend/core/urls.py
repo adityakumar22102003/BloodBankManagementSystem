0709425c9be601e466ac5f8adbee11fd0c2c9a50
Adityakumar22102003 <aditya.bcastudent22.15762@cimage.in>
2025-06-20 11:44:08 +0530

    signup and login api integrated successfully

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns=[
     path('user-details/', views.UserDetailsView.as_view(), name='user-details'),
     path('donor-details/', views.DonorDetailsView.as_view(), name='donor-details'),
+    path('signup/', views.UserSignupAPIView.as_view(), name='signup'),
+    path('login/', views.UserLoginAPIView.as_view(), name='login'),
     # path('blood-inventory/', views.BloodInventoryView.as_view(), name='blood-inventory'),
     # path('blood-request/', views.BloodRequestView.as_view(), name='blood-request'),
     # path('transfusion-record/', views.TransfusionRecordView.as_view(), name='transfusion-record'),
--- backend/core/views.py
+++ backend/core/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from  django.http import JsonResponse
 from .models import *
 from .serializers import *
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password,check_password
 from rest_framework import status
 
 
@@ -15,14 +15,14 @@ class UserDetailsView(APIView):
         serializer=UserDetailsSerializer(users,many=True)
         return JsonResponse(serializer.data,safe=False)        
     
-    def post(self, request):
-        serializer=UserDetailsSerializer(data=request.data)
-        if serializer.is_valid():
-            validated_data=serializer.validated_data
-            validated_data['password']=make_password(validated_data['password'])
-            user=UserDetails.objects.create(**validated_data)
-            return JsonResponse(serializer.data,status=status.HTTP_201_CREATED)
-        return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request):
+    #     serializer=UserDetailsSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         validated_data=serializer.validated_data
+    #         validated_data['password']=make_password(validated_data['password'])
+    #         user=UserDetails.objects.create(**validated_data)
+    #         return JsonResponse(serializer.data,status=status.HTTP_201_CREATED)
+    #     return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
 class DonorDetailsView(APIView):
     def get(self, request):
@@ -38,3 +38,35 @@ class DonorDetailsView(APIView):
             donor=DonorDetails.objects.create(**validated_data)
             return JsonResponse(serializer.data,status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class UserSignupAPIView(APIView):
+    def post(self, request):
+        serializer = UserSignupSerializer(data=request.data)
+        if serializer.is_valid():
+            validated_data=serializer.validated_data
+            validated_data['password']=make_password(validated_data['password'])
+            user=UserDetails.objects.create(**validated_data)
+            # serializer.save()
+            return JsonResponse({'message': 'User signed up successfully'}, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserLoginAPIView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            try:
+                users = UserDetails.objects.filter(email=serializer.validated_data['email'])
+                if not users.exists():
+                    return JsonResponse({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+                if users.count() > 1:
+                    return JsonResponse({'error': 'Multiple users found with this email. Please contact support.'}, status=status.HTTP_400_BAD_REQUEST)
+                user = users.first()
+                if check_password(serializer.validated_data['password'], user.password):
+                    return JsonResponse({'message': 'Login successful', 'userId': user.userId})
+           
+                else:
+                    return JsonResponse({'error': 'Invalid password'}, status=status.HTTP_401_UNAUTHORIZED)
+            except Exception as e:
+                return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
